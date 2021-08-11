1f91f76701fb8ffb3969a5756f88003fee9917b4
charlesagyemang <micnkru@gmail.com>
2021-08-11 08:17:37 +0000

    remove links from beneath

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,5 +1,6 @@
 class LeadersController < ApplicationController
   before_action :set_leader, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /leaders or /leaders.json
   def index
