b6c3ed6c66590af11dc43fcb92ae666b4b3dc402
charlesagyemang <micnkru@gmail.com>
2021-08-23 10:54:56 +0000

    new test mutextes and non multi threading oprion

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,13 +3,19 @@ class Member < ApplicationRecord
 
     def self.get_all_valid_numbers
         phone_number_arrays = []
+        rejected_array = []
         all_members = Member.where.not(phone_number: nil)
         all_members.map  do |member|
             name = member.name.split(" ")[0]
             number = member.phone_number.gsub(" ", "")
             number_starts_with_zero_and_is_ten_digits = number.split("")[0] == "0" && number.length == 10
-            phone_number_arrays.push({name: name, number: number}) if number_starts_with_zero_and_is_ten_digits
+            if number_starts_with_zero_and_is_ten_digits
+                phone_number_arrays.push({name: name, number: number})
+            else 
+                rejected_array.push({name: name, number: number})
+            end
         end
+        puts "======= REJECTED ARRAY ========== #{rejected_array}"
         phone_number_arrays
     end
     
