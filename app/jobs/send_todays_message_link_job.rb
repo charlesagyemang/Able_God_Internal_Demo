125cbf22d067e232efffcf35da1d82a2c5f87fca
charlesagyemang <micnkru@gmail.com>
2021-08-23 05:20:03 +0000

    add new shit insode model to handle stuff

--- app/jobs/send_todays_message_link_job.rb
+++ app/jobs/send_todays_message_link_job.rb
@@ -3,5 +3,6 @@ class SendTodaysMessageLinkJob < ApplicationJob
 
   def perform(*args)
     puts "Send Sermon Link To Customers"
+    puts Member.get_all_valid_numbers
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,11 @@
 class Member < ApplicationRecord
     has_many :leaders, dependent: :destroy
+
+    def self.get_all_valid_numbers
+        phone_number_arrays = []
+        all_members = Member.where.not(phone_number: nil)
+        all_members.map {|member| phone_number_arrays.push(member.phone_number.gsub(" ", "")) if member.phone_number.gsub(" ", "").length == 10 }
+        phone_number_arrays
+    end
+    
 end
