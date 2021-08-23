d081fcc79c32067873ca9aafe63824f28c9332ee
charlesagyemang <micnkru@gmail.com>
2021-08-23 10:48:21 +0000

    new test mutextes and non multi threading oprion

--- app/jobs/send_sms_test_job.rb
+++ app/jobs/send_sms_test_job.rb
@@ -1,11 +1,12 @@
 class SendSmsTestJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    numbers = ["0507565349", "0500443209"]
-    client = WITTLY_FLOW_CLIENT
-    res = client.send_sms("A.G.B.A", numbers[0], "First AGBA Message From Heroku Via Jobs")
-    puts "============ SENDING TEST SMS LOGS: #{res} ====================="
+  def perform(name, number, day="sunday's", by="Bishop Mavis Opoku-Agyemang")
+    message = "Dear #{name}, you can find the link to #{day} message [#{Date.yesterday}] by #{by} using the link below \n https://anchor.fm/bishop-moa\n Scroll down to where you see all the audios and click on play to listen. Please share and follow. God bless you and have a great week dear."
+    puts "============ \n\n\n\n ====================="
+    puts "============ SENDING SMS TO #{name} ON #{number} SMS LOGS: #{message} ====================="
+    puts "============ \n\n\n\n ====================="
+
   end
 
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -3,18 +3,17 @@ namespace :send_message do
     desc "SEND MESSAGE TO MEMBERS"
     task sermon_link: :environment do
       sms_list_hash = Member.get_all_valid_numbers
-      threads = []
       sms_list_hash.each do |member|
-        threads << Thread.new do   
           SendTodaysMessageLinkJob.perform_later(member[:name], member[:number])
-        end
       end
-      threads.map(&:join)
     end
 
     desc "TEST SEND SMS"
     task test: :environment do
-       SendSmsTestJob.perform_later
+      sms_list_hash = Member.get_all_valid_numbers
+      sms_list_hash.each do |member|
+          SendSmsTestJob.perform_later(member[:name], member[:number])
+      end
     end
 
 end
