4361be2b4980bb82000d8a9cbbbc0ff094d62dd8
charlesagyemang <micnkru@gmail.com>
2021-08-24 05:35:08 +0000

    priority

--- app/jobs/send_quote_of_the_day_job.rb
+++ app/jobs/send_quote_of_the_day_job.rb
@@ -2,7 +2,7 @@ class SendQuoteOfTheDayJob < ApplicationJob
   queue_as :default
 
   def perform(name, number, quote)
-    message = "Hi #{name}, #{quote} \n -  Word Of The Day From Bishop Mavis Opoku-Agyemang of A.G.B.A"
+    message = "Hi #{name}, #{quote} \n -  This Is A Message From Bishop Mavis Opoku-Agyemang Of A.G.B.A Stay Blessed"
     client =  WITTLY_FLOW_CLIENT
     res = client.send_sms("A.G.B.A", number, message)
     puts "============ SENDING SMS TO #{name} ON #{number} SMS LOGS: #{message} ====================="
