ea45a4177fbbffad4f7630fa1e8aeb227c81bb3f
charlesagyemang <micnkru@gmail.com>
2021-08-28 08:27:34 +0000

    oliympus

--- app/models/lineup.rb
+++ app/models/lineup.rb
@@ -1,6 +1,6 @@
 class Lineup < ApplicationRecord
     belongs_to :service
-    has_many :programme, dependent: :destroy
+    has_many :programme, -> { order(created_at: :asc) }, dependent: :destroy
 
     def type_with_date
         "#{service.service_type}[#{service.date}] - PLU"
