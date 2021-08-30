b02764352140da1a680d3514a0bc7de0267dce6c
charlesagyemang <micnkru@gmail.com>
2021-08-30 05:03:23 +0000

    olam

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,7 +3,7 @@ require 'resque/server'
 if Rails.env.development?
     Resque.redis = Redis.new(host: 'localhost', port: '6379')
 else 
-    # uri = URI.parse(ENV['REDIS_URL'])
-    # REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-    # Resque.redis = REDIS
+    uri = URI.parse(ENV['REDIS_URL'])
+    REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+    Resque.redis = REDIS
 end
